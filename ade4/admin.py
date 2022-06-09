cb2e2ef40028aba4936b125b5d73caa6087fc675
Zebson2 <cpstatproba@gmail.com>
2022-06-09 17:10:39 +0200

    Skill model

--- ade4/admin.py
+++ ade4/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models as m
 # Register your models here.
+
+
+# admin.site.register(m.Skill)
+
+@admin.register(m.Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
--- ade4/models.py
+++ ade4/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Skill(TimestampMixin):
+    title = models.CharField(verbose_name='Skill', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title.upper()
