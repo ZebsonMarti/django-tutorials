58309ec4e582c08d9e391bec99e94853de24c003
CPSTAT Work-EORTC <cpstatproba@gmail.com>
2022-06-17 14:10:34 +0200

    registered models in admin site

--- bookshtmx/admin.py
+++ bookshtmx/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book
+
+
+class BookInlineAdmin(admin.TabularInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInlineAdmin]
+
+
+admin.site.register(Author, AuthorAdmin)
