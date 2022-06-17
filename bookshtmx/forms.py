5eb7e85d253c0a4b963d78278586358f9963cde6
CPSTAT Work-EORTC <cpstatproba@gmail.com>
2022-06-17 14:20:37 +0200

    Created BookForm and BookFormSet

---
+++ bookshtmx/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Book, Author
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "number_of_pages"]
+
+
+BookFormSet = inlineformset_factory(
+    parent_model=Author, model=Book, form=BookForm, can_delete=False, min_num=2, extra=0
+)
